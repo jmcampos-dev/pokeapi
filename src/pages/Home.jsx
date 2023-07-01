f2e60b4f63849f3a729b6e311322edc3f511b87d
José María <108521775+jmcamposdev@users.noreply.github.com>
2023-07-01 08:04:51 +0200

    Development (#6)
    * Test Github Actions
    
    * Fixed unused imports
    
    * Fixed unused imports
    
    * Change namespaces
    
    * Change the / path to /pokeapi Test
    
    * Added basename to the router

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,7 @@ import Pokemon from "../components/Pokemon/Pokemon.jsx";
 export default function Home() {
   return (
     <>
-      <BrowserRouter>
+      <BrowserRouter basename="/pokeapi">
         <Routes>
           <Route path="/pokeapi" element={<App />} />
           <Route path="/pokemon/:name" element={<Pokemon />} />
