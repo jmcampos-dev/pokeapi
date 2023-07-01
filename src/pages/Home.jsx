60139f510428ccf96399c6bc52cc1ca88353e997
José María <108521775+jmcamposdev@users.noreply.github.com>
2023-07-01 08:03:41 +0200

    Added basename to the router

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,9 +6,9 @@ import Pokemon from "../components/Pokemon/Pokemon.jsx";
 export default function Home() {
   return (
     <>
-      <BrowserRouter>
+      <BrowserRouter basename="/pokeapi">
         <Routes>
-          <Route path="/pokeapi" element={<App />} />
+          <Route path="/" element={<App />} />
           <Route path="/pokemon/:name" element={<Pokemon />} />
         </Routes>
       </BrowserRouter>
